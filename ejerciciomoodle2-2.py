43075ffe285746ce30afd4a8b43dd3eceaa27a61
sparra07 <sammypm2017@gmail.com>
2025-08-06 10:13:16 -0500

    ejerciciomoodle2-2.py

--- ejerciciomoodle2-2.py
+++ ejerciciomoodle2-2.py
@@ -4,12 +4,12 @@ class Producto:
         self.precio = precio
         self.cantidad_disponible = cantidad_disponible
 
-    def ventas(self, num_ventas):
-        if num_ventas > 5:
+    def compras(self, num_compras):
+        if num_compras > 5:
             print("El sistema solo permite comprar 5 unidades de cada producto")
-        elif self.cantidad_disponible >= num_ventas:
-            self.cantidad_disponible - num_ventas 
-            print("Venta de", num_ventas, "unidades de:", self.nombre)
+        elif self.cantidad_disponible >= num_compras:
+            self.cantidad_disponible - num_compras 
+            print("Venta de", num_compras, "unidades de:", self.nombre)
         else:
             print("Venta inválida: no hay suficientes productos de:", self.nombre)
 
@@ -21,33 +21,33 @@ productos = [
     Producto("galletas oreo", 1500, 2)
 ]
 
-#Productos con índice y cantidad
+#Productos con índice
 print("Productos disponibles:")
-print("Índice 0:", productos[0].nombre, "$",productos[0].precio, productos[0].cantidad_disponible, "unidades")
-print("Índice 1:", productos[1].nombre, "$",productos[0].precio, productos[1].cantidad_disponible, "unidades")
-print("Índice 2:", productos[2].nombre, "$",productos[0].precio, productos[2].cantidad_disponible, "unidades")
-print("Índice 3:", productos[3].nombre, "$",productos[0].precio, productos[3].cantidad_disponible, "unidades")
+print("0:", productos[0].nombre, "$",productos[0].precio, productos[0].cantidad_disponible, "unidades")
+print("1:", productos[1].nombre, "$",productos[0].precio, productos[1].cantidad_disponible, "unidades")
+print("2:", productos[2].nombre, "$",productos[0].precio, productos[2].cantidad_disponible, "unidades")
+print("3:", productos[3].nombre, "$",productos[0].precio, productos[3].cantidad_disponible, "unidades")
 
 # Elegir producto por índice
-indice = int(input("Elige el índice del producto que deseas comprar: "))
+indice = int(input("Elige el índice del producto quieres comprar comprar: "))
 
 if indice == 0:
     cantidad = int(input("¿cuantas unidadees quieres comprar?: "))
-    productos[0].ventas(cantidad)
+    productos[0].compras(cantidad)
 elif indice == 1:
     cantidad = int(input("¿cuantas unidadees quieres comprar?: "))
-    productos[1].ventas(cantidad)
+    productos[1].compras(cantidad)
 elif indice == 2:
     cantidad = int(input("¿cuantas unidadees quieres comprar?: "))
-    productos[2].ventas(cantidad)
+    productos[2].compras(cantidad)
 elif indice == 3:
     cantidad = int(input("¿cuantas unidadees quieres comprar?: "))
-    productos[3].ventas(cantidad)
+    productos[3].compras(cantidad)
 else:
     print("Índice incorrecto.")
 
 print("Productos restantes:")
-print("Índice 0:", productos[0].nombre, "$",productos[0].precio, productos[0].cantidad_disponible, "unidades")
-print("Índice 1:", productos[1].nombre, "$",productos[0].precio, productos[1].cantidad_disponible, "unidades")
-print("Índice 2:", productos[2].nombre, "$",productos[0].precio, productos[2].cantidad_disponible, "unidades")
-print("Índice 3:", productos[3].nombre, "$",productos[0].precio, productos[3].cantidad_disponible, "unidades")
+print("0:", productos[0].nombre, "$",productos[0].precio, productos[0].cantidad_disponible, "unidades")
+print("1:", productos[1].nombre, "$",productos[0].precio, productos[1].cantidad_disponible, "unidades")
+print("2:", productos[2].nombre, "$",productos[0].precio, productos[2].cantidad_disponible, "unidades")
+print("3:", productos[3].nombre, "$",productos[0].precio, productos[3].cantidad_disponible, "unidades")
