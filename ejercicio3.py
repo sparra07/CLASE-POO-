a85c259476441e35cd5d969c85843855844edca6
sparra07 <sammypm2017@gmail.com>
2025-09-08 02:48:28 +0000

    taller

---
+++ ejercicio3.py
@@ -0,0 +1,5 @@
+class CuentaBancaria:
+    def __init__(self, numero_cuenta, titular, saldo_inicial):
+        self.numero_cuenta = numero_cuenta
+        self.titular = titular
+        self.saldo = saldo_inicial
