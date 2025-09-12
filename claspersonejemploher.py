a18299ebf6c109a3c90bb06cd3e08cc1fbd49071
sparra07 <sammypm2017@gmail.com>
2025-09-12 11:27:36 -0500

    callse viernes 12

---
+++ claspersonejemploher.py
@@ -0,0 +1,18 @@
+class Persona:
+    def __init__(self,nombre):
+        self.nombre=nombre
+
+    def saludar(self):
+        pass
+
+class estudiante(Persona):
+    def __init__(self, nombre,programa_que_estudia):
+        super().__init__(nombre)
+        self.programa=programa_que_estudia
+
+    def estudiar(self):
+        print(f"{self.nombre} esta estudiando")
+
+persona1=Persona("juan", "in sistemas")
+persona1.saludar
+        
---
+++ her.py
@@ -0,0 +1,38 @@
+class Animal:
+    def __init__(self,nombre):
+        self.nombre= nombre
+
+    def hacer_sonido(self):
+        pass
+
+    def orinar(self):
+         print(f"{self.nombre} esta orinando")
+
+class Perro(Animal):
+    def __init__(self, nombre, color_pelota):
+        super().__init__(nombre)
+        self.color_pelota=color_pelota
+
+    def hacer_sonido(self):
+        print(f"{self.nombre} hace un guau guau")
+
+    def salir_a_pasear(self):
+        print(f"{self.nombre} esta paseando")
+
+class Gato(Animal):
+    def hacer_sonido(self):
+        print(f"{self.nombre} hace miau miau")
+
+animal1 = Perro("mia", "rojo")
+animal1.hacer_sonido()
+animal1.salir_a_pasear()
+animal1.orinar()
+
+animal2 = Gato("simba")
+animal2.hacer_sonido()
+animal2.orinar()      
+        
+print(isinstance(animal1,Perro))
+print(isinstance(animal1,Animal))
+print(issubclass(Perro, Animal))
+print(issubclass(Gato, Animal))
